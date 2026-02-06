16e2776b960fd19845d35800a30e4686ebaf6f4f
Theodore Ch <Theodore.Ch@candi.gr>
2026-02-06 11:43:24 +0200

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-      plugins: [react()],
-      base: '/testCamera/'   // <-- critical for GitHub Pages
+  plugins: [react()],
+  base: "/cameraTest/", // important for GitHub Pages
 });
